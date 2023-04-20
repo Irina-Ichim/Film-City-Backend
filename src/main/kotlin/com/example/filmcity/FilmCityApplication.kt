0d8b3531c41c848cccd3fcded753085bb69ef5c7
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-04-20 15:07:43 +0200

    first commit

---
+++ src/main/kotlin/com/example/filmcity/FilmCityApplication.kt
@@ -0,0 +1,11 @@
+package com.example.filmcity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FilmCityApplication
+
+fun main(args: Array<String>) {
+    runApplication<FilmCityApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/filmcity/FilmCityApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.filmcity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FilmCityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
