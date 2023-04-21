85a05f14330a34cac4843da7b18afa35913c2589
TatianaKedrova <127103022+TatianaKedrova@users.noreply.github.com>
2023-04-21 13:49:37 +0200

    Controller classes + application properties

--- src/main/kotlin/com/example/filmcity/controllers/MovieController.kt
+++ src/main/kotlin/com/example/filmcity/controllers/MovieController.kt
@@ -1,11 +1,13 @@
 package com.example.filmcity.controllers
 
+import com.example.filmcity.repositories.Movie
+import com.example.filmcity.repositories.MovieRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MovieController(private val movieRepository: MovieRepository) {
-    @GetMapping("/localhost:8080/movies")
+    @GetMapping("/movies")
     fun allMovies(): List<Movie?>? {
         return movieRepository.findAll()
     }
--- src/main/kotlin/com/example/filmcity/repositories/Movie.kt
+++ src/main/kotlin/com/example/filmcity/repositories/Movie.kt
@@ -4,13 +4,12 @@ import jakarta.persistence.*
 
 @Table(name = "movies")
 @Entity
-
 data class Movie(
         var title: String,
-        var coverImage: String,
-        var director: String,
-        var year: Int,
-        var synopsis: String,
+        //var coverImage: String,
+        //var director: String,
+        //var year: Int,
+        //var synopsis: String,
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
