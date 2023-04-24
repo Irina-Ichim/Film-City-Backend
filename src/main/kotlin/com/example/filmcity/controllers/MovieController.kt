ca5dbf89c4ea8d85cb7254c7c0e7822adb41f127
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-04-24 10:14:56 +0200

    nada

--- src/main/kotlin/com/example/filmcity/controllers/MovieController.kt
+++ src/main/kotlin/com/example/filmcity/controllers/MovieController.kt
@@ -3,39 +3,44 @@ import com.example.filmcity.repositories.MovieRepository
 import org.springframework.web.bind.annotation.*
 
 
-    @RestController
-    class MovieController(private val movieRepository: MovieRepository) {
+@RestController
+class MovieController(private val movieRepository: MovieRepository) {
 
-        @GetMapping("")
-        fun getMovies(): List<Movie> {
-            return movieRepository.findAll()
-        }
-
-        @PostMapping
-        fun addMovie(@RequestBody movie: Movie): Movie {
-            return movieRepository.save(movie)
-        }
-
-        @PostMapping("/jurassic-park")
-        fun addJurassicPark(): Movie {
-            val jurassicPark = Movie(
-                    title = "Jurassic Park",
-                    director = "Steven Spielberg",
-                    releaseYear = 1993
-            )
-            return movieRepository.save(jurassicPark)
-        }
-
-        @PostMapping("/ratatouille")
-        fun addRatatouille(): Movie {
-            val ratatouille = Movie(
-                    title = "Ratatouille",
-                    director = "Brad Bird",
-                    releaseYear = 2007
-            )
-            return movieRepository.save(ratatouille)
-        }
+    @GetMapping("movie")
+    fun getMovies(): List<Movie> {
+        return movieRepository.findAll()
     }
+
+    @GetMapping("hola")
+    fun sayhi(): String {
+        return "Hola a todos "
+    }
+
+    @PostMapping
+    fun addMovie(@RequestBody movie: Movie): Movie {
+        return movieRepository.save(movie)
+    }
+
+    @PostMapping("/jurassic-park")
+    fun addJurassicPark(): Movie {
+        val jurassicPark = Movie(
+            title = "Jurassic Park",
+            director = "Steven Spielberg",
+            releaseYear = 1993
+        )
+        return movieRepository.save(jurassicPark)
+    }
+
+    @PostMapping("/ratatouille")
+    fun addRatatouille(): Movie {
+        val ratatouille = Movie(
+            title = "Ratatouille",
+            director = "Brad Bird",
+            releaseYear = 2007
+        )
+        return movieRepository.save(ratatouille)
+    }
+}
 
 
 
