46b2682a220230d0ee717bbc2a0fa2369d9bef15
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-04-28 11:30:06 +0200

    errores tests fase 1

--- src/test/kotlin/com/example/filmcity/FilmCityApplicationTests.kt
+++ src/test/kotlin/com/example/filmcity/FilmCityApplicationTests.kt
@@ -54,12 +54,12 @@ class com.example.filmcity(@Autowired val mockMvc: MockMvc) {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"titulo\": \"Jurassic Park\", \"director\": \"Steven Spielberg\",\"releaseYear\": \"1993\" , \"synopsis\": \"Blablablabla\"}")
         ).andExpect(status().isOk)
-        val coders: List<Coder> = coderRepository.findAll()
+        val movies: List<ContenedorPeli> = movieRepository.findAll()
         assertThat(
-            coders, contains(
+            movies, contains(
                 allOf(
-                    hasProperty("name", `is`("Laura")),
-                    hasProperty("favouriteLanguage", `is`("PHP"))
+                    hasProperty("titulo", `is`("Jurassic Park")),
+                    hasProperty("director", `is`("Steven Spielberg"))
                 )
             )
         )
@@ -68,33 +68,33 @@ class com.example.filmcity(@Autowired val mockMvc: MockMvc) {
     @Test
     @Throws(Exception::class)
     fun `allows to find a coder by id`() {
-        val coder: Coder = coderRepository.save(Coder("Marta", "Kotlin"))
-        mockMvc.perform(get("/coders/" + coder.id))
+        val movie: ContenedorPeli = movieRepository.save(ContenedorPeli("Jurassic Park", "Steven Spielberg"))
+        mockMvc.perform(get("/peliculas/" + movie.id))
             .andExpect(status().isOk)
-            .andExpect(jsonPath("$.name", equalTo("Marta")))
-            .andExpect(jsonPath("$.favouriteLanguage", equalTo("Kotlin")))
+            .andExpect(jsonPath("$.titulo", equalTo("Jurassic Park")))
+            .andExpect(jsonPath("$.director", equalTo("Steven Spielberg")))
     }
 
     @Test
     @Throws(Exception::class)
     fun `returns an error if trying to get a coder that does not exist`() {
-        mockMvc.perform(get("/coders/1"))
+        mockMvc.perform(get("/peliculas/1"))
             .andExpect(status().isNotFound())
     }
 
     @Test
     @Throws(Exception::class)
     fun `allows to delete a coder by id`() {
-        val coder: Coder = coderRepository.save(Coder("Marta", "Kotlin"))
-        mockMvc.perform(delete("/coders/" + coder.id))
+        val movie: ContenedorPeli = movieRepository.save(ContenedorPeli("Jurassic Park", "Steven Spielberg"))
+        mockMvc.perform(delete("/peliculas/" + movie.id))
             .andExpect(status().isOk)
-        val coders: List<Coder> = coderRepository.findAll()
+        val movies: List<ContenedorPeli> = movieRepository.findAll()
         assertThat(
-            coders, not(
+            movies, not(
                 contains(
                     allOf(
-                        hasProperty("name", `is`("Marta")),
-                        hasProperty("favouriteLanguage", `is`("Kotlin"))
+                        hasProperty("titulo", `is`("Jurassic Park")),
+                        hasProperty("director", `is`("Steven Spielberg"))
                     )
                 )
             )
@@ -104,44 +104,44 @@ class com.example.filmcity(@Autowired val mockMvc: MockMvc) {
     @Test
     @Throws(Exception::class)
     fun `returns an error if trying to delete a coder that does not exist`() {
-        mockMvc.perform(delete("/coders/1"))
+        mockMvc.perform(delete("/peliculas/1"))
             .andExpect(status().isNotFound())
     }
 
 
     @Test
     @Throws(Exception::class)
-    fun `allows to modify a coder`() {
-        val coder: Coder = coderRepository.save(Coder("Yeraldin", "Java"))
+    fun `allows to modify a movie`() {
+        val movie: ContenedorPeli = movieRepository.save(ContenedorPeli("Jurassic Park", "Steven Spielberg"))
         mockMvc.perform(
-            put("/coders")
+            put("/peliculas")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"id\": \"" + coder.id + "\", \"name\": \"Yeraldin\", \"favouriteLanguage\": \"Ruby\" }")
+                .content("{\"id\": \"" + movie.id + "\", \"titulo\": \"Jurassic Park\", \"director\": \"Steven Spielberg\" }")
         ).andExpect(status().isOk)
-        val coders: List<Coder> = coderRepository.findAll()
-        assertThat(coders, hasSize(1))
-        assertThat(coders[0].name, equalTo("Yeraldin"))
-        assertThat(coders[0].favouriteLanguage, equalTo("Ruby"))
+        val movies: List<ContenedorPeli> = movieRepository.findAll()
+        assertThat(movies, hasSize(1))
+        assertThat(movies[0].titulo, equalTo("Jurassic Park"))
+        assertThat(movies[0].director, equalTo("Steven Spielberg"))
     }
 
     @Test
     @Throws(Exception::class)
     fun `returns an error when trying to modify a coder that does not exist`() {
-        addTestCoders()
+        addTestMovies()
         mockMvc.perform(
-            put("/coders")
+            put("/peliculas")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"id\": \"" + -1 + "\", \"name\": \"Pepita\", \"favouriteLanguage\": \"C++\" }")
+                .content("{\"id\": \"" + -1 + "\", \"titulo\": \"Jurassic Park\", \"director\": \"Steven Spielberg\" }")
         ).andExpect(status().isNotFound())
     }
 
-    private fun addTestCoders() {
-        val coders: List<Coder> = listOf(
-            Coder("Yeraldin", "Java"),
-            Coder("Marta", "Kotlin"),
-            Coder("Daniela", "Python")
+    private fun addTestMovies() {
+        val movies: List<ContenedorPeli> = listOf(
+            ContenedorPeli("Jurassic Park", "Steven Spielberg"),
+            ContenedorPeli("Ratatouille", "Brad Bird"),
+           // Coder("Daniela", "Python")
         )
-        coders.forEach(coderRepository::save)
+        movies.forEach(movieRepository::save)
     }
 
 
