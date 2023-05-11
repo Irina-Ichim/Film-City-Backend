2a29739624483943185b03cf14c3df4c0ab259cb
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-05-12 01:06:57 +0200

    primer test ok

--- src/test/kotlin/com/example/filmcity/FilmCityApplicationTests.kt
+++ src/test/kotlin/com/example/filmcity/FilmCityApplicationTests.kt
@@ -1,10 +1,11 @@
 package com.example.filmcity
 
+
 import com.example.filmcity.repositories.ContenedorPeli
+import com.example.filmcity.repositories.DataLoader
 import com.example.filmcity.repositories.MovieRepository
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
-
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,22 +19,41 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPat
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 
-
 @SpringBootTest(
-    classes = [FilmCityApplicationTests::class],
+    classes = arrayOf(FilmCityApplication::class),
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class FilmCityApplicationTests(@Autowired val mockMvc: MockMvc) {
 
+    @Autowired
+    private lateinit var dataLoader: DataLoader
 
     @Autowired
     private lateinit var movieRepository: MovieRepository
+
 
     @AfterEach
     fun tearDown() {
         movieRepository.deleteAll()
     }
 
+    fun addTestMovies() {
+        val peliculas = listOf(
+            ContenedorPeli(
+                titulo = "Jurassic Park",
+                director = "Steven Spielberg",
+                releaseYear = 1993,
+                synopsis = "Blablablabla"
+            ),
+            ContenedorPeli(
+                titulo = "Ratatouille",
+                director = "Brad Bird",
+                releaseYear = 2007,
+                synopsis = "Blablablabla"
+            )
+        )
+        movieRepository.saveAll(peliculas)
+    }
 
     @Test
     @Throws(Exception::class)
@@ -41,19 +61,15 @@ class FilmCityApplicationTests(@Autowired val mockMvc: MockMvc) {
         addTestMovies()
         mockMvc.perform(get("/peliculas"))
             .andExpect(status().isOk)
-            .andExpect(jsonPath("$[*]", hasSize<Int>(5)))
+            .andExpect(jsonPath("$[*]", hasSize<Int>(4)))
             .andExpect(jsonPath("$[0].titulo", equalTo("Jurassic Park")))
-            .andExpect(jsonPath("$[1].director", equalTo("Steven Spielberg")))
-            .andExpect(jsonPath("$[1].releaseYear", equalTo(1993)))
-            .andExpect(jsonPath("$[2].synopsis", equalTo("Blablablabla")))
+            .andExpect(jsonPath("$[0].director", equalTo("Steven Spielberg")))
+            .andExpect(jsonPath("$[0].releaseYear", equalTo(1993)))
+            .andExpect(jsonPath("$[0].synopsis", equalTo("Blablablabla")))
+            .andExpect(jsonPath("$[1].titulo", equalTo("Ratatouille")))
+            .andExpect(jsonPath("$[1].director", equalTo("Brad Bird")))
+            .andExpect(jsonPath("$[1].releaseYear", equalTo(2007)))
+            .andExpect(jsonPath("$[1].synopsis", equalTo("Blablablabla")))
             .andDo(print())
-    }
-
-    private fun addTestMovies() {
-        val movies: List<ContenedorPeli> = listOf(
-            ContenedorPeli("Jurassic Park", "Steven Spielberg", 1993, "Blablablabla"),
-            ContenedorPeli("Ratatouille", "Brad Bird",2007, "Blablablabla"),
-        )
-        movies.forEach(movieRepository::save)
     }
 }
